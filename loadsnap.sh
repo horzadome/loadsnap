7cecf246669d9ae3223027eedb80e0c61cc15ead
Davor Grubisa <horzadome@gmail.com>
2012-09-19 19:12:25 +0200

    Indentation damn it

--- loadsnap.sh
+++ loadsnap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
-# title		:logsnap.sh
+# title		: logsnap.sh
 # descritption	: A small script to make a server processes and resource snapshot so that it can be analyzed later.
-# author	: Davor Grubisa <horzadome@gmail.com> for jamiiforums.com
+# author	: Davor Grubisa <horzadome@gmail.com>
+# url		: https://github.com/horzadome/loadsnap
 # usage		: just run it, no arguments required
 #
 # version	: v.0.1 - 2012-09-19 Davor Grubisa : written the script
@@ -12,90 +13,89 @@ logdir='/var/log/loadsnap'
 logfile=$logdir'/'$nowstamp
 mkdir -p -m 700 $logdir
 touch $logfile
-(
-
-echo "==========================================="
-echo "Server load from" `hostname -f`
-echo "Taken on " $nowstamp
-echo "==========================================="
-)2>&1 |tee -a $logfile
 
 (
-echo ""
-echo "==========================================="
-echo "           Memory usage (MB)"
-echo "==========================================="
-free -m
+  echo "==========================================="
+  echo "Server load from" `hostname -f`
+  echo "Taken on " $nowstamp
+  echo "==========================================="
+) 2>&1 |tee -a $logfile
+
+(
+  echo ""
+  echo "==========================================="
+  echo "           Memory usage (MB)"
+  echo "==========================================="
+  free -m
 ) 2>&1>> $logfile
 
 (
-echo ""
-echo "==========================================="
-echo "              Network (approx numbers)"
-echo "==========================================="
-echo "Total conn: " `netstat -nap |wc -l`
-echo "HTTP conn : " `netstat -nap |grep :80 |wc -l`
-echo "MySQL conn: " `netstat -nap |grep :3306 |wc -l`
-echo "Bandwidth :"
-bwm-ng -o plain -c 1 -t 3000
+  echo ""
+  echo "==========================================="
+  echo "              Network (approx numbers)"
+  echo "==========================================="
+  echo "Total conn: " `netstat -nap |wc -l`
+  echo "HTTP conn : " `netstat -nap |grep :80 |wc -l`
+  echo "MySQL conn: " `netstat -nap |grep :3306 |wc -l`
+  echo "Bandwidth :"
+  bwm-ng -o plain -c 1 -t 3000
 ) 2>&1>> $logfile
 
 (
-echo ""
-echo "==========================================="
-echo "           CPU load ( last 10 intervals)"
-echo "==========================================="
-sar -q |tail -n 10
+  echo ""
+  echo "==========================================="
+  echo "           CPU load ( last 10 intervals)"
+  echo "==========================================="
+  sar -q |tail -n 10
 ) 2>&1>> $logfile
 
 (
-echo ""
-echo "==========================================="
-echo "           Disk IO (5s interval)"
-echo "==========================================="
-dstat --noupdate -a 5 1
-) 2>&1>> $logfile
-
-
-(
-echo ""
-echo "==========================================="
-echo "           Top output"
-echo "==========================================="
-top -b -n 1 -c
-) 2>&1>> $logfile
-
-(
-echo ""
-echo "==========================================="
-echo "           Process list"
-echo "==========================================="
-ps axuf
-) 2>&1>> $logfile
-
-(
-echo ""
-echo "==========================================="
-echo "           Apache processes"
-echo "==========================================="
-service httpd fullstatus
-) 2>&1>> $logfile
-
-(
-echo ""
-echo "==========================================="
-echo "           MySQL processes"
-echo "==========================================="
-mysqladmin processlist
+  echo ""
+  echo "==========================================="
+  echo "           Disk IO (5s interval)"
+  echo "==========================================="
+  dstat --noupdate -a 5 1
 ) 2>&1>> $logfile
 
 
 (
-echo ""
-echo "==========================================="
-echo "Snapshot done and saved as "
-echo $logfile 
-echo "==========================================="
+  echo ""
+  echo "==========================================="
+  echo "           Top output"
+  echo "==========================================="
+  top -b -n 1 -c
+) 2>&1>> $logfile
+
+(
+  echo ""
+  echo "==========================================="
+  echo "           Process list"
+  echo "==========================================="
+  ps axuf
+) 2>&1>> $logfile
+
+(
+  echo ""
+  echo "==========================================="
+  echo "           Apache processes"
+  echo "==========================================="
+  service httpd fullstatus
+) 2>&1>> $logfile
+
+(
+  echo ""
+  echo "==========================================="
+  echo "           MySQL processes"
+  echo "==========================================="
+  mysqladmin processlist
+) 2>&1>> $logfile
+
+(
+  echo ""
+  echo "==========================================="
+  echo "Snapshot done and saved as "
+  echo $logfile
+  echo "==========================================="
 ) 2>&1 |tee -a $logfile
 
 exit 0
